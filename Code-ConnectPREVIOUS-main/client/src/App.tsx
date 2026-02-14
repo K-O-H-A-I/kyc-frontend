cbed6de9940b14cdc96e36687ac414fd828def19
LakshyaSood123 <newmaillakshya@gmail.com>
2026-02-14 18:27:24 +0530

    Fix router base for Pages

--- Code-ConnectPREVIOUS-main/client/src/App.tsx
+++ Code-ConnectPREVIOUS-main/client/src/App.tsx
@@ -1,4 +1,4 @@
-import { Switch, Route } from "wouter";
+import { Router as WouterRouter, Switch, Route } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
@@ -10,12 +10,14 @@ import Home from "@/pages/Home";
 import "@/styles/tokens.css";
 import "@/styles/ui.css";
 
-function Router() {
+function AppRouter() {
   return (
-    <Switch>
-      <Route path="/" component={Home} />
-      <Route component={NotFound} />
-    </Switch>
+    <WouterRouter base={import.meta.env.BASE_URL}>
+      <Switch>
+        <Route path="/" component={Home} />
+        <Route component={NotFound} />
+      </Switch>
+    </WouterRouter>
   );
 }
 
@@ -23,7 +25,7 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
-        <Router />
+        <AppRouter />
         <Toaster />
       </TooltipProvider>
     </QueryClientProvider>
